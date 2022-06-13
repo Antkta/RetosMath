193921638d1ea95ecd0569e33714ebb07222aee8
ricardoja <107321857+ricardoja@users.noreply.github.com>
2022-06-12 19:18:29 -0500

    agrego tabla 3, 4, 5

---
+++ src/TablaCinco.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class TablaCinco {
+
+    HashMap<String, Integer> TablaCinco = new HashMap<>();
+
+    public TablaCinco(){
+
+        TablaCinco.put("Cinco por Uno", 5);
+        TablaCinco.put("Cinco por Dos", 10);
+        TablaCinco.put("Cinco por Tres", 15);
+        TablaCinco.put("Cinco por Cuatro", 20);
+        TablaCinco.put("Cinco por Cinco", 25);
+        TablaCinco.put("Cinco por Seis", 30);
+        TablaCinco.put("Cinco por Siete", 35);
+        TablaCinco.put("Cinco por Ocho", 40);
+        TablaCinco.put("Cinco por Nueve", 45);
+        TablaCinco.put("Cinco por Diez", 50);
+
+
+    }
+}
